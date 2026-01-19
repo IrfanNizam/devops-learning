19aab29bfc02ba00e3e630db7d49ea2357dd04a2
Irfan Nizam <moirfannizam@gmail.com>
2026-01-19 23:00:50 +0800

    fix: incorect disk usage command

--- server-stats/stats.sh
+++ server-stats/stats.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 
-awk '/^cpu / {usage=($2+$4)*100/($2+$4+$5)} END {printf "CPU Usage: %f%\n", usage}' /proc/stat
+echo "\nTotal CPU usage:"
+awk '/^cpu / {usage=($2+$4)*100/($2+$4+$5)} END {printf "\n%f%", usage}' /proc/stat
 
+echo "\nTotal memory usage:"
 awk '/MemTotal/ {t=$2} /MemAvailable/ {a=$2} END {
 u=t-a;
-printf "Used: %.2f%% (%d MB) | Free: %.2f%% (%d MB)\n",
+printf "Used: %.2f%% (%d MB) | Free: %.2f%% (%d MB)",
 u/t*100, u/1024, a/t*100, a/1024
 }' /proc/meminfo
 
-awk '/MemTotal/ {t=$2} /MemAvailable/ {a=$2} END {
-u=t-a;
-printf "Used: %.2f%% (%d MB) | Free: %.2f%% (%d MB)\n",
-u/t*100, u/1024, a/t*100, a/1024
-}' /proc/meminfo
+echo "\nTotal Disk Usage:"
+df -h / | awk 'NR==2 {
+printf "Total: %s | Used: %s (%s) | Free: %s\n", $2, $3, $5, $4
+}'
 
-echo "\nTop 5 processes by CPU usage"
+echo "\nTop 5 processes by CPU usage:"
 ps -eo pid,cmd,%cpu --sort=-%cpu | sed 1d | head -n 5
 
-echo "\nTop 5 processes by Memory usage"
+echo "\nTop 5 processes by memory usage:"
 ps -eo pid,ppid,cmd,%mem,rss --sort=-%mem | head -n 5
