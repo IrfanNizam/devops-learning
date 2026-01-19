a9cfa48ade5034ad3b401d3975fdce4251e80361
Irfan Nizam <moirfannizam@gmail.com>
2026-01-19 23:09:48 +0800

    feat: add more info

--- server-stats/stats.sh
+++ server-stats/stats.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
+source /etc/os-release
+echo "OS: $PRETTY_NAME\n"
+
+awk '{print "Uptime (seconds):\n", int($1)}' /proc/uptime
+awk '{print "Load average (1m 5m 15m):\n", $1, $2, $3}' /proc/loadavg
+
 echo "\nTotal CPU usage:"
-awk '/^cpu / {usage=($2+$4)*100/($2+$4+$5)} END {printf "\n%f%", usage}' /proc/stat
+awk '/^cpu / {usage=($2+$4)*100/($2+$4+$5)} END {printf "%f%\n", usage}' /proc/stat
 
 echo "\nTotal memory usage:"
 awk '/MemTotal/ {t=$2} /MemAvailable/ {a=$2} END {
 u=t-a;
-printf "Used: %.2f%% (%d MB) | Free: %.2f%% (%d MB)",
+printf "Used: %.2f%% (%d MB) | Free: %.2f%% (%d MB)\n",
 u/t*100, u/1024, a/t*100, a/1024
 }' /proc/meminfo
 
@@ -20,3 +26,9 @@ ps -eo pid,cmd,%cpu --sort=-%cpu | sed 1d | head -n 5
 
 echo "\nTop 5 processes by memory usage:"
 ps -eo pid,ppid,cmd,%mem,rss --sort=-%mem | head -n 5
+
+echo "\nLogged-in users:"
+who | awk '{print $1}' | sort | uniq
+echo ""
+echo "Last 5 failed login attempts:"
+sudo tail -n 5 /var/log/auth.log
