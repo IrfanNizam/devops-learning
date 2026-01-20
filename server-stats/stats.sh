6332ae3e3c2192ef198aa4c5d3b21c5e08959797
Irfan Nizam <moirfannizam@gmail.com>
2026-01-20 08:21:01 +0800

    feat: update scripts, add learnings

--- server-stats/stats.sh
+++ server-stats/stats.sh
@@ -1,34 +1,60 @@
 #!/bin/bash
 
-source /etc/os-release
-echo "OS: $PRETTY_NAME\n"
+echo "###############"
+echo "# System Info #"
+echo "###############"
 
-awk '{print "Uptime (seconds):\n", int($1)}' /proc/uptime
-awk '{print "Load average (1m 5m 15m):\n", $1, $2, $3}' /proc/loadavg
+. /etc/os-release
+echo "OS: $PRETTY_NAME"
+echo "Uptime: $(uptime)"
 
-echo "\nTotal CPU usage:"
-awk '/^cpu / {usage=($2+$4)*100/($2+$4+$5)} END {printf "%f%\n", usage}' /proc/stat
+echo
 
-echo "\nTotal memory usage:"
-awk '/MemTotal/ {t=$2} /MemAvailable/ {a=$2} END {
-u=t-a;
-printf "Used: %.2f%% (%d MB) | Free: %.2f%% (%d MB)\n",
-u/t*100, u/1024, a/t*100, a/1024
-}' /proc/meminfo
+echo "###################"
+echo "# Total CPU Usage #"
+echo "###################"
 
-echo "\nTotal Disk Usage:"
-df -h / | awk 'NR==2 {
-printf "Total: %s | Used: %s (%s) | Free: %s\n", $2, $3, $5, $4
-}'
+top -bn1 | grep "%Cpu(s):" | cut -d ',' -f 4 | awk '{print "Usage: " 100-$1 "%"}'
 
-echo "\nTop 5 processes by CPU usage:"
+echo
+
+echo "######################"
+echo "# Total Memory Usage #"
+echo "######################"
+free | grep "Mem:" -w | awk '{printf "Total: %.1fGi\nUsed: %.1fGi (%.2f%%)\nFree: %.1fGi (%.2f%%)\n",$2/1024^2, $3/1024^2, $3/$2 * 100, $4/1024^2, $4/$2 * 100}'
+
+echo
+
+echo "####################"
+echo "# Total Disk Usage #"
+echo "####################"
+# Accounting for reserved space.
+df -h | grep "/" -w | awk '{printf "Total: %sG\nUsed: %s (%.2f%%)\nFree: %s (%.2f%%)\n",$3 + $4, $3, $3/($3+$4) * 100, $4, $4/($3+$4) * 100}'
+
+echo
+
+echo "################################"
+echo "# Top 5 Processes by CPU Usage #"
+echo "################################"
 ps -eo pid,cmd,%cpu --sort=-%cpu | sed 1d | head -n 5
 
-echo "\nTop 5 processes by memory usage:"
+echo
+
+echo "###################################"
+echo "# Top 5 Processes by Memory Usage #"
+echo "###################################"
 ps -eo pid,ppid,cmd,%mem,rss --sort=-%mem | head -n 5
 
-echo "\nLogged-in users:"
+echo
+
+echo "###################"
+echo "# Logged-in users #"
+echo "###################"
 who | awk '{print $1}' | sort | uniq
-echo ""
-echo "Last 5 failed login attempts:"
-sudo tail -n 5 /var/log/auth.log
+
+echo
+
+echo "################################"
+echo "# Last 5 failed login attempts #"
+echo "################################"
+tail -n 5 /var/log/auth.log
